a2e85688623991293b73fdf29ffab483dcedef5f
João Paulo do Nascimento <jjoaop4ulo@gmail.com>
2022-01-30 07:52:47 -0300

    problem 5 'Smallest multiple' solved

---
+++ solution/problem005.c
@@ -0,0 +1,26 @@
+/*
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	int a, b, remainder = 0;
+
+	for (a = 2520; ; a += 10) {
+		for (b = 3; b <= 20; b++) {
+			if (a % b != 0)
+				goto here;
+		}
+		printf("ANSWER = %d\n", a);
+		break;
+
+	here:
+		remainder = 0;
+	}
+
+	return 0;
+}
