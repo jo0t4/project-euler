f0fdead648a3a74e2ef68aeab8b3d8949375bee1
João Paulo do Nascimento <jjoaop4ulo@gmail.com>
2022-02-01 07:37:50 -0300

    improved code reading

--- solution/problem001.c
+++ solution/problem001.c
@@ -10,12 +10,14 @@ int main() {
 
 	int result = 0, multiple_3, multiple_5;
 
-	for (multiple_3 = 0; multiple_3 * 3 < 1000; multiple_3++) {
+	for (multiple_3 = 0; multiple_3 * 3 < 1000; multiple_3++) 
+	{
 		if (multiple_3 % 5 != 0)
 			result += multiple_3 * 3;
 	}
 
-	for (multiple_5 = 0; multiple_5 * 5 < 1000; multiple_5++) {
+	for (multiple_5 = 0; multiple_5 * 5 < 1000; multiple_5++) 
+	{
 		result += multiple_5 * 5;
 	}
 
